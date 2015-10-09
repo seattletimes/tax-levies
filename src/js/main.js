22102dacc5d4f8de41b33c5dc9255851cd56c2f3
Audrey Carlsen <acarlsen@seattletimes.com>
2015-10-09 15:57:48 -0700

    maybe this will make IE happy?

--- src/js/main.js
+++ src/js/main.js
@@ -7,16 +7,31 @@ require("angular");
 
 var app = angular.module("tax-levies", []);
 
-var housing = '#d94801';
-var families = '#f16913';
-var seattleCenter = '#fdae6b';
-var parks = '#6a51a3';
-var transportation = "#807dba";
-var other ='#bcbddc';
+var c1 = "#2F2933";
+var c2 = "#53516E";
+var c3 = "#7D7AA6";
+var c4 = "#6578A6";
+var c5 = "#5FBAD9";
+var c6 = "#01A2A6";
+var c7 = "#29D9C2";
+var c8 = "#92F2B6";
+var c9 = "#BDF271";
+var c10 = "#FFE878";
+
+var housing = c10;
+var art = c9;
+var families = c8;
+var seattleCenter = c7;
+var parks = c6;
+var fire = c5;
+var transportation = c4;
+var market = c3;
+var library = c2;
+var preschool = c1;
 
 var levyLookup = {
   "1986 Low Income Housing": housing,
-  "1990 Seattle Art Museum": other,
+  "1990 Seattle Art Museum": art,
   "1990 Families/Ed": families,
   "1991 Seattle Center/Comm Ctrs": seattleCenter,
   "1995 Low Income Housing": housing,
@@ -24,15 +39,17 @@ var levyLookup = {
   "1999 Seattle Center/Comm Ctrs": seattleCenter,
   "2000 Parks for All": parks,
   "2002 Low Income Housing": housing,
-  "2004 Fire Facilities": other,
+  "2004 Fire Facilities": fire,
   "2004 Families/Ed": families,
   "2006 Bridging the Gap Transportation": transportation,
-  "2009 Pike Place Market": other,
+  "2009 Pike Place Market": market,
   "2008 Parks": parks,
   "2009 Housing": housing,
   "2011 Families/Ed": families,
-  "2012 Library": other,
-  "2014 Preschool": families
+  "2012 Library": library,
+  "2014 Preschool": preschool,
+  "2016 Seattle Park District": parks,
+  "2016 Move Seattle Transportation": transportation
 };
 
 // find maximum
